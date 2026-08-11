9d388afba7bf2731eec7df7ffe5f15f00abf0213
So Muzaffari <252221750+muzaff-beep@users.noreply.github.com>
2026-08-11 07:52:42 +0400

    i

--- ui-presentation/src/main/kotlin/com/watermelon/ui/screens/SettingsScreen.kt
+++ ui-presentation/src/main/kotlin/com/watermelon/ui/screens/SettingsScreen.kt
@@ -34,7 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.graphicsLayer
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
