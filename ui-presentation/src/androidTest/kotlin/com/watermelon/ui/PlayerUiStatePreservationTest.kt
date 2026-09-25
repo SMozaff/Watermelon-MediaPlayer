406838db6ad8797cb2c0c9f77fbaf28694681832
Soheil Mozaffari <Soheil.Mozaffari@gmail.com>
2026-09-25 21:33:49 +0330

    fix(test): migrate player UI test to Compose API v2

--- ui-presentation/src/androidTest/kotlin/com/watermelon/ui/PlayerUiStatePreservationTest.kt
+++ ui-presentation/src/androidTest/kotlin/com/watermelon/ui/PlayerUiStatePreservationTest.kt
@@ -2,7 +2,7 @@ package com.watermelon.ui
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.click
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
